25251f19afe869286b85c4ae860e8a24ffc5809f
Mist Gitlab Runner <gitlab.ops.mist.io@gmail.com>
2022-06-24 11:57:10 +0000

    Regenerate mist-sdk-go after: Fix Rule examples

--- model_edit_rule_request.go
+++ model_edit_rule_request.go
@@ -17,8 +17,6 @@ import (
 
 // EditRuleRequest struct for EditRuleRequest
 type EditRuleRequest struct {
-	// The name of the rule
-	Name *string `json:"name,omitempty"`
 	// The description of the rule
 	Description *string `json:"description,omitempty"`
 	Selectors []Selector `json:"selectors,omitempty"`
@@ -43,38 +41,6 @@ func NewEditRuleRequest() *EditRuleRequest {
 func NewEditRuleRequestWithDefaults() *EditRuleRequest {
 	this := EditRuleRequest{}
 	return &this
-}
-
-// GetName returns the Name field value if set, zero value otherwise.
-func (o *EditRuleRequest) GetName() string {
-	if o == nil || o.Name == nil {
-		var ret string
-		return ret
-	}
-	return *o.Name
-}
-
-// GetNameOk returns a tuple with the Name field value if set, nil otherwise
-// and a boolean to check if the value has been set.
-func (o *EditRuleRequest) GetNameOk() (*string, bool) {
-	if o == nil || o.Name == nil {
-		return nil, false
-	}
-	return o.Name, true
-}
-
-// HasName returns a boolean if a field has been set.
-func (o *EditRuleRequest) HasName() bool {
-	if o != nil && o.Name != nil {
-		return true
-	}
-
-	return false
-}
-
-// SetName gets a reference to the given string and assigns it to the Name field.
-func (o *EditRuleRequest) SetName(v string) {
-	o.Name = &v
 }
 
 // GetDescription returns the Description field value if set, zero value otherwise.
@@ -271,9 +237,6 @@ func (o *EditRuleRequest) SetTriggerAfter(v TriggerAfter) {
 
 func (o EditRuleRequest) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
-	if o.Name != nil {
-		toSerialize["name"] = o.Name
-	}
 	if o.Description != nil {
 		toSerialize["description"] = o.Description
 	}
